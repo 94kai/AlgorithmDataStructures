a6dad7d1c7046aa8a26102588ee96da1117a3ff3
薛凯 <3440395@qq.com>
2017-04-15 08:28:49 +0800

    泛型之前的提交

---
+++ code/DataStructures/app/src/main/java/com/xk/chapter1/No1_3_1.java
@@ -0,0 +1,46 @@
+package com.xk.chapter1;
+
+
+import static com.xk.chapter1.No1_1_2.s;
+
+/**
+ * 现有一个方法，printDigit,只能打印一个正一位数，请写一个方法print，用来打印任意一个正整数
+ * Created by X.Sation on 2017/4/14.
+ */
+
+public class No1_3_1 {
+    public static void main(String[] args) {
+        print(1234567890);
+    }
+
+
+    private static void print(int num) {
+        if (isDigit(num)) {//基准情形
+            printDigit(num);
+        }else{
+            //1.打印前面的几位
+            print(num/10);
+            //2.打印最后位
+            print(num%10);
+        }
+    }
+
+
+    /**
+     * 打印一位数
+     *
+     * @param digit
+     */
+    private static void printDigit(int digit) {
+        if (isDigit(digit)) {
+            System.out.print(digit);
+            return;
+        }
+        throw new IllegalStateException("不能输出两位数");
+    }
+
+    private static boolean isDigit(int num) {
+        if (num >= 0 && num < 10) return true;
+        return false;
+    }
+}
