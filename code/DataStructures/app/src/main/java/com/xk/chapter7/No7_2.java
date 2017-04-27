50c4218edc918eb7db2acc6208aeb994d7899ef5
薛凯 <3440395@qq.com>
2017-04-27 22:18:39 +0800

    no message

---
+++ code/DataStructures/app/src/main/java/com/xk/chapter7/No7_2.java
@@ -0,0 +1,29 @@
+package com.xk.chapter7;
+
+import java.util.Random;
+
+/**
+ * Created by xuekai on 2017/4/25.
+ */
+
+public class No7_2 {
+    public static void main(String args[]) {
+
+        Integer[][] a5;
+        a5 = new Integer[3][];
+        for(int i = 0; i < a5.length; i++) {
+            a5[i] = new Integer[3];
+            for(int j = 0; j < a5[i].length; j++)
+                a5[i][j] = new Integer(i*j);
+        }
+        for (int i = 0; i < a5.length; i++) {
+            for (int i1 = 0; i1 < a5[i].length; i1++) {
+                System.out.print(a5[i][i1]);
+            }
+            System.out.println();
+        }
+    }
+}
+class User{
+
+}
